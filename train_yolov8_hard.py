2bc46592f2f4163a68015c00e90d6cc8677e6ce7
orangecorn-star <nightnoeat@163.com>
2026-06-27 08:41:11 +0800

    Update train_yolov8_hard.py

--- train_yolov8_hard.py
+++ train_yolov8_hard.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     results = model.train(
         data='data.yaml', 
         epochs=100, 
-        batch=8,    # 它的显存占用极小，8 batch 完全没问题，甚至可以开到 16
+        batch=8,    
         imgsz=640,
         device=0, 
         workers=0,
-        name='train_yolov8_enhanced' # 给这次的新成果单独建一个文件夹，与之前的区分开
+        name='train_yolov8_enhanced' 
     )
